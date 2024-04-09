8097dc8ac5565da5f6f5f0620ac4f74c4f97827c
CORP\pagarcia <pablo.garcia-simavilla.external@capgemini.com>
2024-04-09 14:34:37 +0200

    Cors enabled

--- Inventory_backend/Inventory/src/main/java/com/mycompany/inventory/controller/CategoryRestController.java
+++ Inventory_backend/Inventory/src/main/java/com/mycompany/inventory/controller/CategoryRestController.java
@@ -2,6 +2,7 @@ package com.mycompany.inventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.mycompany.inventory.model.Category;
 import com.mycompany.inventory.response.CategoryResponseRest;
 import com.mycompany.inventory.services.ICategoryService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryRestController {
