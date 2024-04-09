cab9e1fe855d80d796220b0815133ebee98b1134
CORP\pagarcia <pablo.garcia-simavilla.external@capgemini.com>
2024-04-09 07:53:12 +0200

    CategoryRestController search method

---
+++ Inventory_backend/Inventory/src/main/java/com/mycompany/inventory/controller/CategoryRestController.java
@@ -0,0 +1,24 @@
+package com.mycompany.inventory.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mycompany.inventory.response.CategoryResponseRest;
+import com.mycompany.inventory.services.ICategoryService;
+
+@RestController
+@RequestMapping("/api/v1")
+public class CategoryRestController {
+	
+	@Autowired
+	private ICategoryService iCategoryService;
+	
+	@GetMapping("/categories")
+	public ResponseEntity<CategoryResponseRest> searchCategories(){
+		ResponseEntity<CategoryResponseRest> response = iCategoryService.search();
+		return response;
+	}
+}
