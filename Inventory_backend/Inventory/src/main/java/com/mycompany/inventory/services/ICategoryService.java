c517991d5fc0a085b5dfa100eb2085906df29184
CORP\pagarcia <pablo.garcia-simavilla.external@capgemini.com>
2024-04-09 13:21:25 +0200

    Save category Service & RestController

--- Inventory_backend/Inventory/src/main/java/com/mycompany/inventory/services/ICategoryService.java
+++ Inventory_backend/Inventory/src/main/java/com/mycompany/inventory/services/ICategoryService.java
@@ -2,10 +2,12 @@ package com.mycompany.inventory.services;
 
 import org.springframework.http.ResponseEntity;
 
+import com.mycompany.inventory.model.Category;
 import com.mycompany.inventory.response.CategoryResponseRest;
 
 public interface ICategoryService {
 	
 	public ResponseEntity<CategoryResponseRest> search();
 	public ResponseEntity<CategoryResponseRest> searchById(Long id);
+	public ResponseEntity<CategoryResponseRest> save(Category category);
 }
