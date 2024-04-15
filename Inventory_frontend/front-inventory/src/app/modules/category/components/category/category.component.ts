b2c0489189328ed9824cbc4f1554ea4bc17c55a6
CORP\pagarcia <pablo.garcia-simavilla.external@capgemini.com>
2024-04-15 14:57:11 +0200

    Displaying categories table

--- Inventory_frontend/front-inventory/src/app/modules/category/components/category/category.component.ts
+++ Inventory_frontend/front-inventory/src/app/modules/category/components/category/category.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { CategoryService } from 'src/app/modules/shared/services/category.service';
 
 @Component({
@@ -8,21 +9,41 @@ import { CategoryService } from 'src/app/modules/shared/services/category.servic
 })
 export class CategoryComponent implements OnInit {
 
-  private categoryService = inject(CategoryService);
+  constructor(private categoryService: CategoryService) {} 
 
   ngOnInit(): void {
     this.getCategories();
   }
+
+  displayedColumns: string[] = ['id', 'name', 'description', 'actions'];
+  datasource = new MatTableDataSource<CategoryElement>();
 
   getCategories(): void {
     this.categoryService.getCategories()
       .subscribe({
         next: (data: any) => {
           console.log("Respuesta categories: ", data);
+          this.processCategoriesResponse(data);
         },
         error: (error: any) => {
           console.log("Error: ", error);
         }
       });
   }
+
+  processCategoriesResponse(resp: any){
+    const dataCategory: CategoryElement[] = [];
+    if(resp.metadata[0].code === "200"){ 
+      let listCategory = resp.categoryResponse.category;
+      listCategory.forEach((element: CategoryElement) => { 
+        dataCategory.push(element);
+      });
+      this.datasource = new MatTableDataSource<CategoryElement>(dataCategory); 
+    }
+  }
+}
+export interface CategoryElement{
+    description: string;
+    id: number;
+    name: string;
 }
