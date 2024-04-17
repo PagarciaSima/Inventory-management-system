0834aa76bbfa0a3d410b1f33edfbee39b85b7f60
CORP\pagarcia <pablo.garcia-simavilla.external@capgemini.com>
2024-04-17 07:56:11 +0200

    Search product by ID controller

--- Inventory_backend/Inventory/src/main/java/com/mycompany/inventory/controller/ProductRestController.java
+++ Inventory_backend/Inventory/src/main/java/com/mycompany/inventory/controller/ProductRestController.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -51,6 +53,12 @@ public class ProductRestController {
 		product.setPrice(price);
 		product.setPicture(Util.compressZLib(picture.getBytes()));
 		ResponseEntity<ProductResponseRest> response = productService.save(product, categoryId);
+		return response;
+	}
+	
+	@GetMapping("/products/{id}")
+	public ResponseEntity<ProductResponseRest> searchById(@PathVariable Long id){
+		ResponseEntity<ProductResponseRest> response = productService.searchById(id);
 		return response;
 	}
 }
