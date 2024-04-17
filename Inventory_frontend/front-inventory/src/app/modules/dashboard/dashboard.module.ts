5c8fcab42c45dfa9d8f1f92f12f80663cddcac83
CORP\pagarcia <pablo.garcia-simavilla.external@capgemini.com>
2024-04-17 16:56:35 +0200

    Angular product module & component & routes

--- Inventory_frontend/front-inventory/src/app/modules/dashboard/dashboard.module.ts
+++ Inventory_frontend/front-inventory/src/app/modules/dashboard/dashboard.module.ts
@@ -5,6 +5,7 @@ import { DashboardComponent } from './pages/dashboard.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { CategoryModule } from '../category/category.module';
+import { ProductModule } from '../product/product.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CategoryModule } from '../category/category.module';
   imports: [
     CommonModule,
     SharedModule,
-    CategoryModule
+    CategoryModule,
+    ProductModule
   ]
 })
 export class DashboardModule { }
