dceb9280c8a6ab850f753ce9c55f3f4579f3b25d
pagarciaSim <pablo.garciasimavilla@gmail.com>
2024-04-22 09:18:11 +0200

    Export excel rest controller backend

--- Inventory_backend/Inventory/src/main/java/com/mycompany/inventory/controller/CategoryRestController.java
+++ Inventory_backend/Inventory/src/main/java/com/mycompany/inventory/controller/CategoryRestController.java
@@ -1,5 +1,7 @@
 package com.mycompany.inventory.controller;
 
+import java.io.IOException;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,88 +16,109 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mycompany.inventory.model.Category;
 import com.mycompany.inventory.response.CategoryResponseRest;
 import com.mycompany.inventory.services.ICategoryService;
+import com.mycompany.inventory.util.CategoryExcelExporter;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+import jakarta.servlet.http.HttpServletResponse;
+
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryRestController {
-	
+
 	private ICategoryService iCategoryService;
-	
+
 	public CategoryRestController(ICategoryService categoryService) {
 		this.iCategoryService = categoryService;
 	}
-	
+
 	/**
 	 * Retrieves all available categories.
 	 *
-	 * @return ResponseEntity<CategoryResponseRest> An HTTP response containing the list of categories.
+	 * @return ResponseEntity<CategoryResponseRest> An HTTP response containing the
+	 *         list of categories.
 	 */
 	@GetMapping("/categories")
-	public ResponseEntity<CategoryResponseRest> searchCategories(){
+	public ResponseEntity<CategoryResponseRest> searchCategories() {
 		ResponseEntity<CategoryResponseRest> response = iCategoryService.search();
 		return response;
 	}
-	
+
 	/**
 	 * Retrieves a specific category by its identifier.
 	 *
 	 * @param id The unique identifier of the category to be searched.
-	 * @return ResponseEntity<CategoryResponseRest> An HTTP response containing the found category.
+	 * @return ResponseEntity<CategoryResponseRest> An HTTP response containing the
+	 *         found category.
 	 */
 	@GetMapping("/categories/{id}")
-	public ResponseEntity<CategoryResponseRest> searchById(@PathVariable Long id){
+	public ResponseEntity<CategoryResponseRest> searchById(@PathVariable Long id) {
 		ResponseEntity<CategoryResponseRest> response = iCategoryService.searchById(id);
 		return response;
 	}
-	
+
 	/**
 	 * Endpoint to save a new category.
 	 * 
-	 * @param category The category object to be saved, provided in the request body.
-	 * @return ResponseEntity containing the response with the saved category 
-	 *         and metadata indicating the success of the operation. If the category 
-	 *         is successfully saved, a 200 OK response will be returned. If the category 
-	 *         is not saved (for example, due to validation errors), a 400 Bad Request 
-	 *         response will be returned.
+	 * @param category The category object to be saved, provided in the request
+	 *                 body.
+	 * @return ResponseEntity containing the response with the saved category and
+	 *         metadata indicating the success of the operation. If the category is
+	 *         successfully saved, a 200 OK response will be returned. If the
+	 *         category is not saved (for example, due to validation errors), a 400
+	 *         Bad Request response will be returned.
 	 */
 	@PostMapping("/categories")
-	public ResponseEntity<CategoryResponseRest> saveCategory(@RequestBody Category category){
+	public ResponseEntity<CategoryResponseRest> saveCategory(@RequestBody Category category) {
 		ResponseEntity<CategoryResponseRest> response = iCategoryService.save(category);
 		return response;
 	}
-	
+
 	/**
 	 * Endpoint to update an existing category by ID.
 	 * 
 	 * @param category The updated category object, provided in the request body.
-	 * @param id The ID of the category to be updated, provided as a path variable.
-	 * @return ResponseEntity containing the response with the updated category 
-	 *         and metadata indicating the success of the operation. If the category 
-	 *         is successfully updated, a 200 OK response will be returned. If the category 
-	 *         is not found, a 404 Not Found response will be returned. If the category 
-	 *         is not updated (for example, due to validation errors), a 400 Bad Request 
-	 *         response will be returned.
+	 * @param id       The ID of the category to be updated, provided as a path
+	 *                 variable.
+	 * @return ResponseEntity containing the response with the updated category and
+	 *         metadata indicating the success of the operation. If the category is
+	 *         successfully updated, a 200 OK response will be returned. If the
+	 *         category is not found, a 404 Not Found response will be returned. If
+	 *         the category is not updated (for example, due to validation errors),
+	 *         a 400 Bad Request response will be returned.
 	 */
 	@PutMapping("/categories/{id}")
-	public ResponseEntity<CategoryResponseRest> updateCategory(@RequestBody Category category,@PathVariable Long id){
+	public ResponseEntity<CategoryResponseRest> updateCategory(@RequestBody Category category, @PathVariable Long id) {
 		ResponseEntity<CategoryResponseRest> response = iCategoryService.update(category, id);
 		return response;
 	}
-	
+
 	/**
 	 * Endpoint to delete a category by its ID.
 	 * 
 	 * @param id The ID of the category to be deleted, provided as a path variable.
-	 * @return ResponseEntity containing the response with metadata indicating 
-	 *         the success of the operation. If the category is successfully deleted, 
-	 *         a 200 OK response will be returned. If the category is not found, 
-	 *         a 404 Not Found response will be returned. If an error occurs during 
-	 *         the deletion process, a 500 Internal Server Error response will be returned.
+	 * @return ResponseEntity containing the response with metadata indicating the
+	 *         success of the operation. If the category is successfully deleted, a
+	 *         200 OK response will be returned. If the category is not found, a 404
+	 *         Not Found response will be returned. If an error occurs during the
+	 *         deletion process, a 500 Internal Server Error response will be
+	 *         returned.
 	 */
 	@DeleteMapping("/categories/{id}")
-	public ResponseEntity<CategoryResponseRest> deleteCategory(@PathVariable Long id){
+	public ResponseEntity<CategoryResponseRest> deleteCategory(@PathVariable Long id) {
 		ResponseEntity<CategoryResponseRest> response = iCategoryService.deleteById(id);
 		return response;
+	}
+
+	@GetMapping("/categories/export/excel")
+	public void exportToExcel(HttpServletResponse response) throws IOException {
+		response.setContentType("application/octet-stream");
+		String headerKey = "Content-Disposition";
+		String headerValue = "attachment; filename=result_categories.xlsx";
+		response.setHeader(headerKey, headerValue);
+
+		ResponseEntity<CategoryResponseRest> categoryResponse = iCategoryService.search();
+		CategoryExcelExporter excelExporter = new CategoryExcelExporter(
+				categoryResponse.getBody().getCategoryResponse().getCategory());
+		excelExporter.export(response);
 	}
 }
